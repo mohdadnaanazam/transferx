96393edb1367674089dd2a021603b3f801ed31f3
alokverma749 <alokverma749@gmail.com>
2024-03-04 23:58:55 +0530

    Aceternity UI setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
-import { Button } from '../components/Button'
+"use client"
+import { NavbarDemo } from "@/components/Navbar"
+import { SparklesCore } from "../components/ui/sparkles"
+import UploadForm from "@/components/UploadForm"
+
 export default function Home() {
   return (
-    <main className='bg-red-900'>
-      HOME
-      <Button />
+    <main>
+      <div className="h-[100vh] relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+        <NavbarDemo />
+        <div className="w-full absolute inset-0 h-screen">
+          <SparklesCore
+            id="tsparticlesfullpage"
+            background="transparent"
+            minSize={0.6}
+            maxSize={1.4}
+            particleDensity={200}
+            className="w-full h-full"
+            particleColor="#FFFFFF"
+          />
+        </div>
+        <UploadForm />
+      </div>
     </main>
-  );
+  )
 }
