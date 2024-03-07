344e34dea11f8d9b641d28033d0ec3d1b6e97cab
alokverma749 <alokverma749@gmail.com>
2024-03-07 17:50:36 +0530

    setup react-dropzone

--- src/components/UploadCard/UploadCard.tsx
+++ src/components/UploadCard/UploadCard.tsx
@@ -1,13 +1,23 @@
 "use client"
-import React from "react"
+import React, { useCallback } from 'react'
+
+import { useDropzone } from 'react-dropzone'
 import { BackgroundGradient } from "../../../ui/background-gradient"
 import CloudUploadOutlinedIcon from '@mui/icons-material/CloudUploadOutlined';
 
 export const UploadCard = () => {
+  const onDrop = useCallback((acceptedFiles : any) => {
+    // Do something with the files
+    console.log(acceptedFiles, 'this is acceptedFiles')
+  }, [])
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
   return (
     <BackgroundGradient className="rounded-[22px] p-1 sm:p-5 border bg-zinc-900">
-      <div className="h-56 border rounded-[22px] flex justify-center items-center">
-        <CloudUploadOutlinedIcon sx={{ fontSize: '150px', color: 'white' }} />
+      <div {...getRootProps()} className="h-56 border rounded-[22px] flex justify-center items-center">
+        <input {...getInputProps()} />
+        { isDragActive ? <p className='text-white text-xl'> Drop here </p>: <CloudUploadOutlinedIcon sx={{ fontSize: '150px', color: 'white' }} /> }
       </div>
 
       <p className="text-base sm:text-xl mt-4 mb-2 text-neutral-200">
