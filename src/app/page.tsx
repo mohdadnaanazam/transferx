96c49178367f2a4c55d52a96a7e78e101719154f
devadnaan <adnaanazam28@gmail.com>
2024-03-10 20:46:39 +0530

    fix: minor fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,71 +1,8 @@
-'use client'
-
 import { CardWithForm } from '@/components/CardWithForm'
 import axios from 'axios'
 import { useState } from 'react'
 
 export default function Page() {
-  const [file, setFile] = useState<File | null>(null)
-  const [uploading, setUploading] = useState(false)
-  const [shareLink, setShareLink] = useState('')
-  const [progress, setProgress] = useState<null | number>(0)
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-
-    if (!file) {
-      alert('Please select a file to upload.')
-      return
-    }
-
-    setUploading(true)
-
-    const response = await fetch(
-      process.env.NEXT_PUBLIC_BASE_URL + '/api/upload',
-      {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ filename: file.name, contentType: file.type }),
-      }
-    )
-
-    const signedUploadResponse = await response.json()
-
-    const signedURL = signedUploadResponse.url
-    const objectKey = signedUploadResponse.key
-
-
-    const formData = new FormData();
-    formData.append('file', file);
-
-    await axios.put(signedURL, file, {
-      headers: {
-        'Content-Type': file.type
-      },
-      onUploadProgress: (progressEvent: any) => {
-        if (progressEvent.bytes) {
-          setProgress(Math.round((progressEvent.loaded / progressEvent.total) * 100))
-        }
-      },
-    })
-      .then(response => {
-        console.log('File uploaded successfully:', response);
-      })
-      .catch(error => {
-        console.error('Error uploading file:', error);
-      });
-
-    const getSignedURL = await fetch(process.env.NEXT_PUBLIC_BASE_URL + `/api/get-url?key=${objectKey}`, {
-      method: 'GET'
-    })
-
-    const shareableURL = await getSignedURL.json()
-    setShareLink(shareableURL.url)
-
-  }
-
-  console.log(`${process.env.NEXT_PUBLIC_BASE_URL}/${shareLink}`)
-
   return (
     <main className='flex justify-between flex-row'>
       <div className='w-1/2 flex justify-center items-center'>
@@ -93,7 +30,7 @@ export default function Page() {
       </div>
       <div className='w-1/2 flex justify-center items-center flex-row space-x-4 h-[75vh] relative'>
         <div className='bg-green-700 w-2 h-20 mt-4'></div>
-        <h1 className='text-8xl font-medium'>transferr</h1>
+        <h1 className='text-[140px] font-medium'>transferr</h1>
         <p className='absolute bottom-52 right-52 text-sm font-semibold'>where files fly faster</p>
       </div>
     </main>
--- src/components/CardWithForm/CardWithForm.tsx
+++ src/components/CardWithForm/CardWithForm.tsx
@@ -6,36 +6,103 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { DatePicker } from "../DatePicker"
 import { CirclePlus, Lock } from "lucide-react"
+import { useRef, useState } from "react"
+import axios from "axios"
+import { Progress } from "../ui/progress"
 
 export const CardWithForm = () => {
+  // init
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  // states
+  const [file, setFile] = useState<File | null>(null)
+  const [uploading, setUploading] = useState(false)
+  const [shareLink, setShareLink] = useState('')
+  const [progress, setProgress] = useState<null | number>(0)
+
+  const handleSubmit = async () => {
+
+    if (!file) {
+      alert('Please select a file to upload.')
+      return
+    }
+
+    setUploading(true)
+
+    const response = await fetch(
+      process.env.NEXT_PUBLIC_BASE_URL + '/api/upload',
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ filename: file.name, contentType: file.type }),
+      }
+    )
+
+    const signedUploadResponse = await response.json()
+
+    const signedURL = signedUploadResponse.url
+    const objectKey = signedUploadResponse.key
+
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    await axios.put(signedURL, file, {
+      headers: {
+        'Content-Type': file.type
+      },
+      onUploadProgress: (progressEvent: any) => {
+        if (progressEvent.bytes) {
+          setProgress(Math.round((progressEvent.loaded / progressEvent.total) * 100))
+        }
+      },
+    })
+      .then(response => {
+        console.log('File uploaded successfully:', response);
+      })
+      .catch(error => {
+        console.error('Error uploading file:', error);
+      });
+
+    const getSignedURL = await fetch(process.env.NEXT_PUBLIC_BASE_URL + `/api/get-url?key=${objectKey}`, {
+      method: 'GET'
+    })
+
+    const shareableURL = await getSignedURL.json()
+    setShareLink(shareableURL.url)
+
+  }
+
   return (
     <Card className="w-[450px]">
       <CardHeader>
-        <CardTitle className="flex items-center">
+        <CardTitle onClick={() => inputRef?.current?.click()} className="flex items-center cursor-pointer">
           <CirclePlus size={30} strokeWidth={1.25} className="mr-2 my-2 text-green-700" />
           Upload files
         </CardTitle>
         <CardDescription>Or select a folder</CardDescription>
-        {/*
-        <form onSubmit={handleSubmit}>
-          <input
-            id="file"
-            type="file"
-            onChange={(e) => {
-              const files = e.target.files
-              if (files) {
-                setFile(files[0])
-              }
-            }}
-            accept="image/png, image/jpeg, application/pdf, video/mp4, video/mpeg, video/quicktime"
-          />
-          <button type="submit" disabled={uploading}>
-            Upload
-          </button>
-        </form>
+        <input
+          id="file"
+          type="file"
+          ref={inputRef}
+          onChange={(e) => {
+            const files = e.target.files
+            if (files) {
+              setFile(files[0])
+            }
+          }}
+          className="hidden"
+          accept="image/png, image/jpeg, application/pdf, video/mp4, video/mpeg, video/quicktime"
+        />
+
 
         {shareLink && <a target='_blank' href={`${process.env.NEXT_PUBLIC_BASE_URL}/${shareLink}`}>{`${process.env.NEXT_PUBLIC_BASE_URL}/${shareLink}`}</a>}
-        {progress && <p>{progress}%</p>} */}
+        
+        <div className="flex items-center gap-4">
+          <Progress value={progress} />
+          <p className="font-bold">{progress}%</p>
+        </div>
+
       </CardHeader>
       <CardContent>
         <form>
@@ -53,7 +120,7 @@ export const CardWithForm = () => {
       </CardContent>
       <CardFooter className="flex justify-between">
         <Button variant="outline" className="space-x-2 p-3 flex flex-row justify-between w-32"> Lock with pin <Lock size={16} strokeWidth={1.25} /></Button>
-        <Button>Generate Link</Button>
+        <Button onClick={handleSubmit}>Generate Link</Button>
       </CardFooter>
     </Card>
   )
