9adc4d49d5467d41b567c03e16293fdf17c003c3
alokverma749 <alokverma749@gmail.com>
2024-03-17 00:01:14 +0530

    fix: theme icon toggle

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -11,8 +11,8 @@ export const ThemeToggle = () => {
 
   return (
     <Button variant="ghost" size="icon" onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
-      <Sun className="h-[1.5rem] w-[1.3rem] dark:hidden" />
-      <Moon className="hidden h-5 w-5 dark:block" />
+      <Sun className="hidden h-[1.5rem] w-[1.3rem] dark:block" />
+      <Moon className="h-5 w-5 dark:hidden" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   )
