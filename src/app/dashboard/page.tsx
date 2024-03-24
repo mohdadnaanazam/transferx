19be85ba878f4a6344a28b012ba2edc967a8a198
alokverma749 <alokverma749@gmail.com>
2024-03-24 19:28:35 +0530

    trivial

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,6 @@
-import { Sidebar } from "@/components/dashboard/sidebar";
+import { Sidebar } from "@/components/dashboard/sidebar"
+import { Button } from "@/components/ui/button"
+import { Plus } from "lucide-react"
 
 export default async function Dashboard() {
 
@@ -6,6 +8,9 @@ export default async function Dashboard() {
     <main className="flex justify-between h-full mt-1">
       <Sidebar />
       <div className="w-10/12 border">main</div>
+      <Button variant="outline" size="icon">
+        <Plus className="h-4 w-4" />
+      </Button>
     </main>
   )
 }
--- src/components/dashboard/sidebar/Sidebar.tsx
+++ src/components/dashboard/sidebar/Sidebar.tsx
@@ -34,5 +34,5 @@ export const Sidebar = () => {
       ))}
     </ul>
   </div>
-  );
-};
+  )
+}
