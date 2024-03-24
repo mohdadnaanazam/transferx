f681e37cc281c6da58cab63ad8b2eb63356e27c5
devadnaan <adnaanazam28@gmail.com>
2024-03-24 19:00:07 +0530

    fix: add loading during login

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,9 +1,17 @@
 'use client'
 
+import { LoaderIcon } from "lucide-react"
 import { useSession } from "next-auth/react"
 import { useRouter } from "next/navigation"
 import { createContext, useEffect } from "react"
 
+const Loading = () => {
+  return (
+    <div className="flex h-screen flex-1 justify-center items-center">
+      <LoaderIcon className="animate-spin h-24 ml-1" />
+    </div>
+  )
+}
 
 export const AuthContext = createContext<{}>({ session: {}, status: 'loading' })
 
@@ -20,9 +28,9 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     }
   }, [status, session])
 
-  console.log('session', session)
-
-
-
-  return <AuthContext.Provider value={{ session, status }}>{children}</AuthContext.Provider>
+  return (
+    <AuthContext.Provider value={{ session, status }}>
+      {status === 'loading'? <Loading /> : children}
+    </AuthContext.Provider>
+  )
 }
