f82a4e7a9ba4d0a67701d5b8dad684994d083992
sachinchauhan010 <010sssachin@gmail.com>
2024-06-17 12:38:52 +0530

    remove copy right line

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,10 +2,8 @@ import React from 'react'
 
 export function Footer() {
   return (
-    <section className='flex justify-around items-center px-10 py-2 text-base font-medium'>
-      <p className=''>made with ❤️ in india</p>
-      <p className=''>© 2024 Copyright: <span className='font-semibold'>transferX.in</span></p>
-    </section>
+    
+      <p className='px-16 py-2 text-base font-medium'>made with ❤️ in india</p>
   )
 }
 
