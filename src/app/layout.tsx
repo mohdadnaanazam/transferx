688abc0b4bcc475e060a26f0cc733f7d71bf3357
sachinchauhan010 <010sssachin@gmail.com>
2024-06-17 12:23:40 +0530

    mode with love in india

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import { ThemeProvider } from '../components/Providers/Theme/ProviderTheme'
 import { Toaster } from "@/components/ui/toaster"
 import { AuthProvider } from "@/context/auth-context"
 import { SessionProvider } from "next-auth/react"
+import { Footer } from '@/components/footer'
 
 interface RootLayoutProps {
   children: React.ReactNode
@@ -27,6 +28,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
                 <div className="relative h-full flex min-h-screen flex-col">
                   <Header />
                   <div className="flex-1">{children}</div>
+                  <Footer/>
                 </div>
               </AuthProvider>
             </ThemeProvider>
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export function Footer() {
+  return (
+    <section className='flex justify-around items-center px-10 py-2 text-base font-medium'>
+      <p className=''>made with ❤️ in india</p>
+      <p className=''>© 2024 Copyright: <span className='font-semibold'>transferX.in</span></p>
+    </section>
+  )
+}
+
+
