885155806a6fd816fc6fec1556f67bbfe1654657
mohdadnaanazam <adnaan28.azam@gmail.com>
2024-07-14 16:32:25 +0530

    chore: add button variants

--- src/app/(guest)/download/link-expired/page.tsx
+++ src/app/(guest)/download/link-expired/page.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 
 import type { Metadata } from 'next'
+import { BorderMagicButton } from '@/components/Buttons'
 
 export const metadata: Metadata = {
   title: 'Link Expired',
@@ -18,11 +19,8 @@ export default function ExpiredLink() {
           <p className="text-2xl font-semibold md:text-3xl">Sorry, This link is expired.</p>
           <p className="mt-4 mb-8 dark:text-gray-600">But dont worry, you can find plenty of other things on our homepage.</p>
 
-          <Link href='/' className="relative inline-flex h-12 overflow-hidden rounded-md p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-md bg-slate-950 px-5 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-              Back to home
-            </span>
+          <Link href='/'>
+            <BorderMagicButton>Back to home</BorderMagicButton>
           </Link>
         </div>
       </div>
---
+++ src/components/Buttons/BorderMagic/BorderMagicButton.tsx
@@ -0,0 +1,18 @@
+import type { ReactNode, ButtonHTMLAttributes } from "react";
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+}
+
+export const BorderMagicButton = (props: Props) => {
+  const { children, ...rest } = props;
+
+  return (
+    <button {...rest} className="relative inline-flex h-12 overflow-hidden rounded-md p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+      <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-md bg-slate-950 px-5 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+        {children}
+      </span>
+    </button>
+  );
+};
