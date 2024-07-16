5f8095b6fd1cbe96726df468f170507267387758
sachinchauhan010 <010sssachin@gmail.com>
2024-07-16 21:58:39 +0530

    shimmer added for link info table

--- src/components/pages/Links/Links.tsx
+++ src/components/pages/Links/Links.tsx
@@ -8,6 +8,7 @@ import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, Tabl
 import { handleDownload } from "@/components/DownloadCard/DownloadCard"
 import { PreviewPanel } from "@/components/PreviewPanel"
 import { useToast } from "@/components/ui/use-toast"
+import TableShimmer from "@/components/TableShimmer/TableShimmer"
 
 export const Links = () => {
   const { toast } = useToast()
@@ -19,48 +20,52 @@ export const Links = () => {
     }).catch((err) => {
       toast({ title: "Error", description: "Unable to copy link to clipboard." })
     })
-  }
 
-  return (
-    <Table className="mt-6">
-      <TableCaption>{(links?.length > 0) ? '' : 'No data available'}</TableCaption>
+    if (links?.length <= 0) {
+      return <TableShimmer />;
+    }
 
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">Status</TableHead>
-          <TableHead>Name</TableHead>
-          <TableHead>Download URL</TableHead>
-          <TableHead>Expiry</TableHead>
-          <TableHead>Shareable URL</TableHead>
-          <TableHead />
-        </TableRow>
-      </TableHeader>
+    return (
+      <Table className="mt-6">
+        <TableCaption>{(links?.length > 0) ? '' : 'No data available'}</TableCaption>
 
-      <TableBody>
-        {links?.map((link) => (
-          <TableRow key={link.id}>
-            <TableCell className="font-medium">
-              {(new Date(link?.expiryDate) < new Date()) ? <X className="text-red-500" /> : <Check className="text-green-500" />}
-            </TableCell>
-
-            <TableCell>{link?.name}</TableCell>
-
-            <TableCell>
-              <ArrowDownToLine className="cursor-pointer ml-6" onClick={() => handleDownload(link?.downloadURL, link?.name, link?.file_type)} />
-            </TableCell>
-
-            <TableCell>{new Date(link?.expiryDate).toLocaleDateString()}</TableCell>
-
-            <TableCell className="cursor-pointer">
-              <Copy className="ml-6" onClick={() => handleCopy(link?.shortURL, link.id)} />
-            </TableCell>
-
-            <TableCell>
-              <PreviewPanel url={link?.previewURL} type={link?.file_type} downloadableURL={link?.downloadURL} fileName={link?.name} handleDownload={handleDownload} />
-            </TableCell>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Status</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Download URL</TableHead>
+            <TableHead>Expiry</TableHead>
+            <TableHead>Shareable URL</TableHead>
+            <TableHead />
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  )
+        </TableHeader>
+
+        <TableBody>
+          {links?.map((link) => (
+            <TableRow key={link.id}>
+              <TableCell className="font-medium">
+                {(new Date(link?.expiryDate) < new Date()) ? <X className="text-red-500" /> : <Check className="text-green-500" />}
+              </TableCell>
+
+              <TableCell>{link?.name}</TableCell>
+
+              <TableCell>
+                <ArrowDownToLine className="cursor-pointer ml-6" onClick={() => handleDownload(link?.downloadURL, link?.name, link?.file_type)} />
+              </TableCell>
+
+              <TableCell>{new Date(link?.expiryDate).toLocaleDateString()}</TableCell>
+
+              <TableCell className="cursor-pointer">
+                <Copy className="ml-6" onClick={() => handleCopy(link?.shortURL, link.id)} />
+              </TableCell>
+
+              <TableCell>
+                <PreviewPanel url={link?.previewURL} type={link?.file_type} downloadableURL={link?.downloadURL} fileName={link?.name} handleDownload={handleDownload} />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    )
+  }
 }
