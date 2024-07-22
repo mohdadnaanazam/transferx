2155419f05a03be9704e96f4d86ada038cee284b
alokverma <alokverma749@gmail.com>
2024-07-23 01:16:35 +0530

    fix: add assets and folders

---
+++ src/app/(main)/(dashboard)/me/assets/page.tsx
@@ -0,0 +1,20 @@
+import DashboardCard from "@/components/DashboardCard/DashboardCard"
+
+export const Assets = ({ }) => {
+
+  return (
+    <div className="flex flex-1">
+      <div className="p-2 md:p-10 rounded-tl-md  border-neutral-200 bg-white dark:bg-[#020817] flex flex-col gap-2 flex-1 w-full h-full">
+        <div className="flex flex-wrap justify-start gap-2 w-full m-2">
+          {[...new Array(8)].map((_, i) => (
+            <div key={i} className="w-full sm:w-[48%] lg:w-[32%] h-56 rounded-lg bg-gray-100 dark:bg-neutral-800">
+              <DashboardCard />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Assets
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,29 +1,26 @@
 'use client'
 
 import Link from "next/link"
-import { useTheme } from "next-themes"
 import { signIn, useSession } from "next-auth/react"
 import { Navbar } from "../Navbar"
 
 export const Header = () => {
-  const { setTheme, theme } = useTheme()
-  const { data: session, status } = useSession()
-  // setTheme('dark')
+  const { status } = useSession()
   const isLoggedIn = status === 'authenticated'
 
   return (
-    isLoggedIn ? <Navbar /> : <div className="py-3 flex max-w-7xl w-full md:mx-auto px-5 flex-row justify-between">
-      <div className='flex justify-center items-center flex-row space-x-2'>
-        <div className='bg-green-0 w-[2px] h-7' />
-        <Link className="font-medium text-2xl" href='/'>transferX</Link>
-      </div>
+    isLoggedIn ? <Navbar /> : (
+      <div className="py-3 flex max-w-7xl w-full md:mx-auto px-5 flex-row justify-between">
+        <div className='flex justify-center items-center flex-row space-x-2'>
+          <div className='bg-green-0 w-[2px] h-7' />
+          <Link className="font-medium text-2xl" href='/'>transferX</Link>
+        </div>
 
-      <div className="flex flex-row gap-3 justify-around items-center">
-        <Link href='/pricing'>Pricing</Link>
-        <button onClick={() => signIn()}>Login</button>
-
-        {/* <ThemeToggle /> */}
+        <div className="flex flex-row gap-3 justify-around items-center">
+          <Link href='/pricing'>Pricing</Link>
+          <button onClick={() => signIn()}>Login</button>
+        </div>
       </div>
-    </div>
+    )
   )
 }
