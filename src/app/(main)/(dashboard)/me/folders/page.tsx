7df37648eb8da41e357e679070e093bb764cf395
sachinchauhan010 <010sssachin@gmail.com>
2024-07-25 17:11:06 +0530

    folder card created

--- src/app/(main)/(dashboard)/me/folders/page.tsx
+++ src/app/(main)/(dashboard)/me/folders/page.tsx
@@ -1,5 +1,5 @@
-import { Folder, EllipsisVertical } from 'lucide-react'
 import DashboardCard from "@/components/DashboardCard/DashboardCard"
+import FolderCard from "@/components/FolderCard/FolderCard"
 export default function Folders() {
 
   return (
@@ -7,17 +7,7 @@ export default function Folders() {
       <div className="p-2 md:p-10 rounded-tl-md  border-neutral-200 bg-white dark:bg-[#020817] flex flex-col gap-2 flex-1 w-full h-full">
         <div className="flex flex-wrap justify-start gap-4 w-full m-2">
           {[...new Array(8)].map((_, i) => (
-            <div key={i} className="w-full sm:w-[48%] md:w-[30%] lg:w-[23%] rounded-lg bg-gray-100 dark:bg-neutral-800 p-2">
-              <div className='flex justify-between items-center'>
-                <button className='flex justify-start gap-x-4'>
-                  <Folder strokeWidth={1}/>
-                  <p>Photos</p>
-                </button>
-                <button>
-                  <EllipsisVertical strokeWidth={1}/>
-                </button>
-              </div>
-            </div>
+            <FolderCard key={i}/>
           ))}
         </div>
       </div>
