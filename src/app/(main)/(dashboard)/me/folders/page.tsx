2f18a2daff2bf6f5cf1001f39a2b1110a83e82c6
alokverma <alokverma749@gmail.com>
2024-07-25 17:49:35 +0530

    refactor

--- src/app/(main)/(dashboard)/me/folders/page.tsx
+++ src/app/(main)/(dashboard)/me/folders/page.tsx
@@ -1,5 +1,5 @@
-import DashboardCard from "@/components/DashboardCard/DashboardCard"
 import FolderCard from "@/components/FolderCard/FolderCard"
+
 export default function Folders() {
 
   return (
@@ -7,7 +7,7 @@ export default function Folders() {
       <div className="p-2 md:p-10 rounded-tl-md  border-neutral-200 bg-white dark:bg-[#020817] flex flex-col gap-2 flex-1 w-full h-full">
         <div className="flex flex-wrap justify-start gap-4 w-full m-2">
           {[...new Array(8)].map((_, i) => (
-            <FolderCard key={i}/>
+            <FolderCard key={i} />
           ))}
         </div>
       </div>
