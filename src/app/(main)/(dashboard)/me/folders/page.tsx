3a94ea333266ed68fc6b85081efe56a129ff1cf1
sachinchauhan010 <010sssachin@gmail.com>
2024-07-26 20:04:50 +0530

    scroll bar removed

--- src/app/(main)/(dashboard)/me/folders/page.tsx
+++ src/app/(main)/(dashboard)/me/folders/page.tsx
@@ -3,8 +3,8 @@ import FolderCard from "@/components/FolderCard/FolderCard"
 export default function Folders() {
 
   return (
-    <div className="p-2 md:p-10 rounded-tl-md border-neutral-200 bg-white dark:bg-[#020817] w-full h-full mr-2">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 md:gap-10 w-full mt-8 md:mt-0 md:m-2 overflow-x-hidden">
+    <div className="p-2 md:p-10 rounded-tl-md border-neutral-200 bg-white dark:bg-[#020817] w-full h-full">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10 w-full">
         {[...new Array(8)].map((_, i) => (
           <FolderCard key={i} />
         ))}
