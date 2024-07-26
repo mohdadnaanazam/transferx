442540eaeb64e4ae848bffe23faa33226fcdf678
alokverma <alokverma749@gmail.com>
2024-07-27 00:32:27 +0530

    fix: 2 header flicker

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,6 +5,7 @@ import { useTheme } from "next-themes"
 import { signIn, useSession } from "next-auth/react"
 
 import { MaxWidthContainer } from "../MaxWidthContainer"
+import { Navbar } from "../Navbar"
 
 export const Header = () => {
   const { setTheme } = useTheme()
@@ -13,17 +14,19 @@ export const Header = () => {
   setTheme('dark')
 
   return (
-    !isLoggedIn &&
-    <MaxWidthContainer className="py-3 flex flex-row justify-between">
-      <div className='flex justify-center items-center flex-row space-x-2'>
-        <div className='bg-green-0 w-[2px] h-7' />
-        <Link className="font-medium text-2xl" href='/'>transferX</Link>
-      </div>
+    isLoggedIn ? <Navbar /> :
+      (
+        <MaxWidthContainer className="py-3 flex flex-row justify-between">
+          <div className='flex justify-center items-center flex-row space-x-2'>
+            <div className='bg-green-0 w-[2px] h-7' />
+            <Link className="font-medium text-2xl" href='/'>transferX</Link>
+          </div>
 
-      <div className="flex flex-row gap-3 justify-around items-center">
-        <Link href='/pricing'>Pricing</Link>
-        <button onClick={() => signIn()}>Login</button>
-      </div>
-    </MaxWidthContainer>
+          <div className="flex flex-row gap-3 justify-around items-center">
+            <Link href='/pricing'>Pricing</Link>
+            <button onClick={() => signIn()}>Login</button>
+          </div>
+        </MaxWidthContainer>
+      )
   )
 }
