4d974db28525f9e96a3c3ead3226c63c5393c0a6
alokverma <alokverma749@gmail.com>
2024-07-26 18:36:31 +0530

    fix: mobile responsiveness

--- src/app/(main)/(dashboard)/me/assets/page.tsx
+++ src/app/(main)/(dashboard)/me/assets/page.tsx
@@ -4,7 +4,7 @@ export default function Assets() {
 
   return (
     <div className="p-2 md:p-10 rounded-tl-md border-neutral-200 bg-white dark:bg-[#020817] w-full h-full">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-3 w-full m-2">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 w-[90%] mt-8 md:mt-0 mx-auto md:mx-0 md:w-full md:m-2">
         {[...new Array(18)].map((_, i) => (
           <div key={i} className="aspect-w-16 aspect-h-9">
             <AssetCard />
--- src/app/(main)/(dashboard)/me/folders/page.tsx
+++ src/app/(main)/(dashboard)/me/folders/page.tsx
@@ -4,7 +4,7 @@ export default function Folders() {
 
   return (
     <div className="p-2 md:p-10 rounded-tl-md border-neutral-200 bg-white dark:bg-[#020817] w-full h-full mr-2">
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10 w-full m-2 overflow-x-hidden">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 md:gap-10 w-full mt-8 md:mt-0 md:m-2 overflow-x-hidden">
         {[...new Array(8)].map((_, i) => (
           <FolderCard key={i} />
         ))}
